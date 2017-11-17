66989f2867aeba7414b37b8c1c3c2dc33fdcb908
achencoms <achencoms@gmail.com>
2017-11-17 13:20:07 -0500

    Add files via upload
    cleaned up code and added total possible points for each

--- AutoGraderTotal/content.js
+++ AutoGraderTotal/content.js
@@ -3,31 +3,43 @@
 
 var competency = 0;
 var proficiency = 0;
+var competencyPossible = 0;
+var proficiencyPossible = 0;
 
 $("td:nth-child(3)").each(function(){
+	var title = $(this).closest("tbody").find(".cat_name").text();
 	var grade = $(this).text().split("/");
 	var total = parseFloat(grade[0]);
 	var totalPossible = parseFloat(grade[1]);
-	if(totalPossible > 10){
-		if(totalPossible == 152){
-			if(total > 150){
-				proficiency += total % 150;
-				competency += 150;
+	if(!title.includes("No Points")){
+		if(totalPossible > 10){
+			if(totalPossible == 152){
+				if(total > 150){
+					proficiency += total % 150;
+					competency += 150;
+				}
+				else competency += total;
+				proficiencyPossible += 2;
+				competencyPossible += 150;
 			}
-			else competency += total;
+			else {
+				competency += total;
+				competencyPossible += totalPossible;
+			}
 		}
-		else if(total >totalPossible){
-			proficiency += total % totalPossible;
-			competency += totalPossible;
-		}else competency += total;
+		else{
+			if($(this).prev().prev().find("a").text() == "Word Play - Part 2") proficiencyPossible += 4; //Word Play Part 2 total is messed up
+			proficiency += total;
+			proficiencyPossible += totalPossible;
+		}
 	}
-	else proficiency += total;
 });
+
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "calculate" ) {
-      alert("Competency Points: " + competency + "\n Proficiency Points: " + proficiency);
+      alert("Competency Points: " + competency  + "/" + competencyPossible + "\n Proficiency Points: " + proficiency + "/" + proficiencyPossible);
     }
   }
 );
